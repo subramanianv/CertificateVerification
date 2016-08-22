a346a6240ee6bcf87b3cdee839fad70b937af970
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2016-08-22 00:59:00 -0400

    Certificate validation using multi-sig

--- CertificateVerfication.sol
+++ CertificateVerfication.sol
@@ -1,19 +1,60 @@
-struct University {
-  string name,
-  bool verified
-}
-uint university_id = 0;
-mapping (address => bool) universityMap ;
-mapping (uint => University) university ;
-
-function addUniversity(_name) {
-  if(universityMap[msg.sender] == true) {
-    return;
+contract certVeri {
+  struct University {
+    bool added;
+    string name;
+    bool verified;
   }
-  else {
-    uint uid = ++university_id;
-    University univ = university[uid];
-    univ.name = _name;
-    univ.verified = false;
+
+  struct Certificate {
+    bool added;
+    string ipfsHash;
+    mapping(uint => uint) owners;
+  }
+
+    modifier onlyUniversity {
+        if (universities[uint(msg.sender)].verified == true)
+            _
+    }
+
+
+  mapping (uint => University) universities;
+  mapping (string => Certificate) certificates;
+
+  function addUniversity (string _name) {
+    if(universities[uint(msg.sender)].added) {
+      return;
+    } else {
+      University univ = universities[uint(msg.sender)];
+      univ.name = _name;
+      univ.verified = true;
+      univ.added = true;
+      universities[uint(msg.sender)] = univ;
+    }
+  }
+
+  function addCertificate (string ipfsHash, address assignee) onlyUniversity {
+    if(certificates[ipfsHash].added == true) return;
+    Certificate cert = certificates[ipfsHash];
+    cert.ipfsHash = ipfsHash;
+    cert.owners[uint(msg.sender)] = 2;
+    cert.owners[uint(assignee)] = 1;
+    certificates[ipfsHash] = cert;
+  }
+
+  function addConfirmation(string ipfsHash) {
+    if(certificates[ipfsHash].added == false) return;
+    Certificate c = certificates[ipfsHash];
+    if(c.owners[uint(msg.sender)] == 0) return;
+    c.owners[uint(msg.sender)] = 2;
+  }
+
+  function isCertificateValid(string ipfsHash, address owner) constant returns (bool) {
+    if(certificates[ipfsHash].added == false) return false;
+    Certificate cert = certificates[ipfsHash];
+    if(cert.owners[uint(owner)] == 2)
+      return true;
+     else{
+       return false;
+     }
   }
 }
