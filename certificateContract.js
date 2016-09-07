c571367755acbeb585437fbf3ac756f95ba91f15
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2016-09-07 02:15:23 -0400

    web3 sign

---
+++ certificateContract.js
@@ -0,0 +1,11 @@
+if (typeof web3 !== 'undefined') {
+    web3 = new Web3(web3.currentProvider);
+} else {
+    // set the provider you want from Web3.providers
+    web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+}
+
+
+function sign(address, msgHash, callback) {
+	web3.eth.sign(address, msgHash, callback);
+}
