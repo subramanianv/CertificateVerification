80488375c06eb4faf00ccf7f393c6514356591b4
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2016-10-19 23:50:43 -0400

    Signature gen

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ var lightwallet = require('eth-lightwallet');
 var keystore = lightwallet.keystore;
 var password = 'mypass';
 var seed = 'ecology face asset comic nephew tragic wisdom clump tray whip affair mobile';
+
 keystore.deriveKeyFromPassword(password, function(err, pwDerivedKey) {
   var ks = new keystore(seed, pwDerivedKey);
   ks.generateNewAddress(pwDerivedKey);
   var address = ks.getAddresses()[0];
   var privateKey = ks.exportPrivateKey(address, pwDerivedKey);
-  console.log(privateKey);
-})
+  var signature = lightwallet.signing.signMsg(ks, pwDerivedKey, "subbu", address);
+  console.log(signature, address, privateKey);
+});
