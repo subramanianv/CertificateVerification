f148eceff90eb284d2fad9ee32cb5c0b968a4211
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2016-10-25 19:09:42 -0400

    uport

--- src/app.js
+++ src/app.js
@@ -7,13 +7,23 @@ var async = require('async');
 var _ = require('underscore');
 var HookedWeb3Provider = require("hooked-web3-provider");
 var utils = require('ethereumjs-util');
-
-
+var uport = require('uport-persona');
+var ipfs = window.IpfsApi('localhost', '5001')
 var TEST_NET = 'https://morden.infura.io/';
 var password = 'mypass';
+var query;
 var seed = 'ecology face asset comic nephew tragic wisdom clump tray whip affair mobile';
 var salt = 'swag';
+var ipfsOptions = {
+  host : 'localhost',
+  port : 5001,
+  protocol : 'http',
+  root : ''
+}
 var keyStore = lightwallet.keystore;
+
+
+
 
 
 
@@ -24,79 +34,50 @@ keyStore.createVault({
     seedPhrase: seed, // Optionally provide a 12-word seed phrase
     salt: salt
 }, function(err, ks) {
-
+  console.log(err);
     ks.passwordProvider = function(callback) {
         callback(null, "mypass");
     };
-
     ks.keyFromPassword(password, function(err, pwDerivedKey) {
         if (err) throw err;
+
         ks.generateNewAddress(pwDerivedKey, 1);
         var addr = ks.getAddresses();
-        addr[0] = '0x' + addr[0];
-        console.log(addr[0]);
+        var myAddress = '0x' + addr[0];
+
         var provider = new HookedWeb3Provider({
             host: TEST_NET,
-            transaction_signer: ks
-        });
-        web3.setProvider(provider);
-        var query = new EthQuery(provider);
-        // //query.gasPrice(console.log);
-        //
-        var simplestorageContract = web3.eth.contract([{
-            "constant": false,
-            "inputs": [{
-                "name": "x",
-                "type": "uint256"
-            }],
-            "name": "set",
-            "outputs": [],
-            "payable": false,
-            "type": "function"
-        }, {
-            "constant": true,
-            "inputs": [],
-            "name": "get",
-            "outputs": [{
-                "name": "retVal",
-                "type": "uint256"
-            }],
-            "payable": false,
-            "type": "function"
-        }]);
-        var simplestorage = simplestorageContract.at("0x4500ff0fcab6949466bb9c0779a7fb2c8c320705");
-        var tx = {
-            from: addr[0]
-        }
-        // var gasPrice = 50000000000
-        // var gas = 3141592
-        // var tx = {
-        //     from: addr[0]
-        // }
-        var args = [20, tx];
-        async.waterfall([
-            function(callback) {
-                query.gasPrice(callback)
-            },
-
-            function estimateGas(gasPrice, callback) {
-                simplestorage.set.estimateGas(100, tx, function(err, gas) {
-                    callback(null, {
-                        gasPrice: gasPrice,
-                        gas : gas
-                    })
+            transaction_signer: {
+              hasAddress : ks.hasAddress.bind(ks),
+              signTransaction : function(txParams, callback) {
+                async.parallel({
+                  gas : function(callback) {
+                    query.estimateGas(txParams, callback)
+                  },
+                  gasPrice : function(callback) {
+                    query.gasPrice(callback)
+                  }
+                }, function(err, result) {
+                  console.log(txParams);
+                  txParams.gas = result.gas;
+                  txParams.gasPrice = result.gasPrice;
+                  ks.signTransaction(txParams,callback);
                 })
-            },
-            function(args, callback) {
-                var tx = {
-                    from: addr[0]
-                }
-                tx = _.extendOwn(tx, args)
-                console.log(tx);
-                callback(null, tx);
+              }
             }
-        ], function(err, tx) {
-            simplestorage.set(101, tx, console.log);
         });
+        var query = new EthQuery(provider);
+        web3.setProvider(provider);
+        debugger;
+        console.log(ks.exportPrivateKey(addr[0], pwDerivedKey));
+        //var persona = new uport.MutablePersona(myAddress, ipfsOptions, web3.currentProvider);
+        //persona.setPublicSigningKey(ks.exportPrivateKey(addr[0], pwDerivedKey));
+        // debugger;
+        //persona.writeToRegistry().then(console.log, console.log);
+        console.log(myAddress);
+        var persona = new uport.Persona(myAddress, ipfsOptions, web3.currentProvider);
+        debugger;
+        persona.load().then(console.log, console.log);
+
     });
 })
