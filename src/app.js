8e51deb940f01400b282d1ff52187072850f442c
Subramanian Venkatesan <subramanian.bsv@gmail.com>
2016-10-31 16:33:32 -0400

    Code cleanup

--- src/app.js
+++ src/app.js
@@ -6,60 +6,84 @@ var EthQuery = require('eth-query');
 var async = require('async');
 var _ = require('underscore');
 var HookedWeb3Provider = require("hooked-web3-provider");
-var utils = require('ethereumjs-util');
 var uport = require('uport-persona');
 var ipfs = window.IpfsApi('localhost', '5001')
-var TEST_NET = 'https://morden.infura.io/';
+var rpcURL = 'https://morden.infura.io/';
 var password = 'mypass';
-var query;
 var keyStore = lightwallet.keystore;
-var bs58 = require('bs58');
 var registryAddress = '0x7356B947b626F48647E648D490C3a4FDe2d11e50';
-var seed = keyStore.generateRandomSeed();
-console.log(seed);
-var pubkey = '0xbf761487a00c156db3f874d010d2fbd39e5bec6a15e7ae3a22a168c21c4e7d3bda5204f71e69b494c2dfe239225940871c99c503922a4746bb6aae61c20b7241';
-
-
-// console.log(seed);
-//var salt = 'swag';
+var seed = 'toe scene suffer upon lottery parent hard glue make profit survey april';
 var ipfsOptions = {
-  host : 'localhost',
-  port : 5001,
-  protocol : 'http',
-  root : ''
+    host: 'localhost',
+    port: 5001,
+    protocol: 'http',
+    root: ''
 }
-web3.setProvider(window.web3.currentProvider);
-var address = web3.eth.accounts[0];
-var persona = new uport.Persona(address, ipfsOptions, web3.currentProvider, registryAddress);
-persona.load().then(function(tokens) {
-  var c = persona.getPublicSigningKey();
-  console.log("pub key", c);
-}, errorlog);
+
+
+function createWeb3Provider(rpcURL, keyStoreInstance) {
+    var query;
+    var provider = new HookedWeb3Provider({
+        host: rpcURL,
+        transaction_signer: {
+            hasAddress: keyStoreInstance.hasAddress.bind(keyStoreInstance),
+            signTransaction: function(txParams, callback) {
+                async.parallel({
+                    gas: function(callback) {
+                        query.estimateGas(txParams, callback);
+                    },
+                    gasPrice: function(callback) {
+                        query.gasPrice(callback);
+                    }
+                }, function(err, result) {
+                    txParams.gas = result.gas;
+                    txParams.gasPrice = result.gasPrice;
+                    keyStoreInstance.signTransaction(txParams, callback);
+                });
+            }
+        }
+    });
+    query = new EthQuery(provider);
+    return provider;
+}
+
+function appInit(args, onSuccess, onError) {
+    keyStore.createVault(args, function(err, ks) {
+        if (err) {
+            onError(err);
+            return;
+        }
+
+        ks.passwordProvider = function(callback) {
+            callback(null, args.password);
+        }
+
+        ks.keyFromPassword(args.password, function(err, pwDerivedKey) {
+            ks.generateNewAddress(pwDerivedKey, 1);
+            var addr = ks.getAddresses();
+            addr = '0x' + addr[0];
+            var provider = createWeb3Provider(rpcURL, ks);
+            onSuccess(addr, pwDerivedKey, ks, provider)
+        });
+
+    });
+}
+
 function errorlog(e) {
-  console.log('error', e);
+    console.log('error', e);
 }
-// var persona = new uport.MutablePersona(address, ipfsOptions, web3.currentProvider, registryAddress);
-// persona.setPublicSigningKey("41e0568d6c95f23e9e8e3a59d289c3888b92f11caa3f0b8438628d0912c0928b");
-// persona.writeToRegistry().then(function(tx) {
-//   console.log('yolo', tx);
-// },console.log);
 
+appInit({
+    password: password,
+    seedPhrase: seed,
+    salt: 'swag'
+}, onReady, errorlog);
 
-// keyStore.createVault({
-//     password: password,
-//     seedPhrase: seed, // Optionally provide a 12-word seed phrase
-//     salt: salt
-// }, function(err, ks) {
-//   console.log(err);
-//     ks.passwordProvider = function(callback) {
-//         callback(null, "mypass");
-//     };
-//     ks.keyFromPassword(password, function(err, pwDerivedKey) {
-//         if (err) throw err;
-//
-//         ks.generateNewAddress(pwDerivedKey, 1);
-//         var addr = ks.getAddresses();
-//         var myAddress = '0x' + addr[0];
-//         debugger;
-//     });
-// })
+function onReady(address, pwDerivedKey, keyStoreInstance, web3Provider) {
+    web3.setProvider(web3Provider);
+    var persona = new uport.Persona(address, ipfsOptions, web3.currentProvider, registryAddress);
+    persona.load().then(function(tokens) {
+        var publicKey = persona.getPublicSigningKey();
+
+    }, errorlog);
+}
